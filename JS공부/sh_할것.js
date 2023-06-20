fdf74965406e0c983190980bdd42db5f5243a48a
heexohee <rsoy2918@naver.com>
2023-06-21 00:20:48 +0900

    헷갈리는 코드

--- JS공부/sh_할것.js
+++ JS공부/sh_할것.js
@@ -3,3 +3,10 @@
 //splice
 // 쉼표 연산자 return,
 //3항 연산자로 조건문 바꿔서 수정하기 연습
+//22번 
+function solution3(n) {
+    let str = n.toString();
+    let nArr = Array.from(str, x => x + x);
+    return nArr[nArr.length - 1];
+  }
+  
